65b59c2eb998cb65a78ba3a6f98d161607c67ba2
Lucas fieri <lucasfieri@gmail.com>
2020-10-20 19:47:04 -0300

    feat: add Raleway font

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -6,7 +6,7 @@ const Head = ({ title }) => (
     <title>{title}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" href="/favicon.ico" />
-
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;400;600;700;800;900&display=swap" rel="stylesheet" />
   </NextHead>
 )
 
