cc74283bdaf901361beca3a365419d5c3bd001b9
Lucas fieri <lucasfieri@gmail.com>
2020-10-21 21:46:09 -0300

    feat: add next images

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 const path = require('path')
+const withImages = require('next-images');
 
-module.exports = {
+module.exports = withImages({
+  serverless: true,
   poweredByHeader: false,
   generateEtags: false,
+  exportTrailingSlash: true,
   webpack: (config, options) => {
     config.plugins = config.plugins || []
     config.node = { fs: 'empty' };
     config.resolve.modules.push(path.resolve(__dirname, 'src'))
     return config
   },
-}
+})
